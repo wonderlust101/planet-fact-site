a18fcff506bb5d7e4d84e4360d12c836aa497d68
Sergei <sergei.borja0107@gmail.com>
2025-05-15 21:13:35 -0600

    Fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-/// <reference types="node" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import * as path from "node:path";
 import { fileURLToPath } from "url";
 import svgr from "vite-plugin-svgr";
 

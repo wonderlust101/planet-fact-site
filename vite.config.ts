5b0f4a1fa1ef1d2f8dbbe773f11e6894060afd6a
Sergei <sergei.borja0107@gmail.com>
2025-05-15 21:00:52 -0600

    Fix tsconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig } from "vite";
+import path from "path";
 import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
